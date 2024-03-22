f05ed7f4ed38340c5b105fdfa5187707ad3e9558
Jake Bram <jhb94@txstate.edu>
2024-03-22 17:28:53 -0500

    Listed Functions
    Wrote the functions expected to be defined in this projects, and the variables they require. Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AIProject2
 Group Project 2, Intro to Artificial Intelligence, Dr. Moonis Ali, Min-Max.
+
+Authors: Jake Bram, Yasha Hockliff, and Gerardo Cantu
+
+This project aims to use Alpha-Beta pruning and the MINIMAX algorithm to simulate
+games of tic-tac-toe. We will simulate different decisions by altering our 
+EVALUATION() function, there for altering how the engine values its moves.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+// FUNCTIONS TO BE DEFINED
+// -----------------------
+
+// MINI-MAX-A-B(position, depth, player, use-thresh, pass-thresh) - AB Pruning, select optimal path.
+
+// MOVEGEN(position, player) - Generate all moves that could be made
+
+// DEEP-ENOUGH(postition, depth) - Has someone won? How many ply have been explored? How promising is this path?
+
+// EVALUATION(position, player) - Return a number that represents a the goodness of players position
