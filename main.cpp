8ea7a1e382b637be0043c3e8ae2c747455f08abb
Jake Bram <jhb94@txstate.edu>
2024-04-01 13:36:43 -0500

    update main
    deleted unused vars

--- main.cpp
+++ main.cpp
@@ -14,14 +14,14 @@ using namespace std;
 
 int main(){
     int EVAL_MAX = 8; // THESE DEPEND ON THE EVAL FUNCTION
-    int EVAL_MIN = -8; // WE WILL USE 100 FOR NOW
-    int depth = 0;
-    string player = "MAX";
+    int EVAL_MIN = -8; 
+
     board position; // BLANK BOARD STATE FOR NEW GAME
     for(int i = 0; i < 9; i++){
         position.positions[i] = 0;
     }
-    position = MINI_MAX_A_B(position, depth, player, EVAL_MAX, EVAL_MIN).second;
+
+    position = MINI_MAX_A_B(position, 0, "MAX", EVAL_MAX, EVAL_MIN).second;
     for(int j = 0; j < 9; j++){
         cout << position.positions[j] << endl;
         if((j+1)%3==0){
